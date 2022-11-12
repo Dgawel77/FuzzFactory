2b5fce1114dfadf6704ccda06d3c7723e3a39e21
David Gawel <dgawel77@gmail.com>
2022-11-11 20:30:54 -0600

    added instrumentation to branching calls

--- llvm_mode/waypoints-branch-pass.cc
+++ llvm_mode/waypoints-branch-pass.cc
@@ -6,11 +6,38 @@ class BranchDepthFeedback : public DomainFeedback<BranchDepthFeedback> {
 public:
     BranchDepthFeedback(Module& M) : DomainFeedback<BranchDepthFeedback>(M, "__afl_branch_dsf") { }
 
-    void visitBasicBlock (BasicBlock &bb) {
+    void visitBranchInst (BranchInst &r) {
+        // check if it is a conditional branch
+        if (r.isConditional()){
+            auto key = createProgramLocation(); // static random value
+
+            // Insert call to `dsf_increment(dsf_map, key, 1)`;
+            auto irb = insert_before(r); // Get a handle to the LLVM IR builder
+            irb.CreateCall(DsfIncrementFunction, {DsfMapVariable, key, getConst(1)}); 
+        }
+    }
+
+    void visitSwitchInst (SwitchInst &r) {
         auto key = createProgramLocation(); // static random value
-        
+
         // Insert call to `dsf_increment(dsf_map, key, 1)`;
-        auto irb = insert_before(bb); // Get a handle to the LLVM IR builder
+        auto irb = insert_before(r); // Get a handle to the LLVM IR builder
+        irb.CreateCall(DsfIncrementFunction, {DsfMapVariable, key, getConst(1)}); 
+    }
+
+    void visitIndirectBranchInst (IndirectBrInst &r) {
+        auto key = createProgramLocation(); // static random value
+
+        // Insert call to `dsf_increment(dsf_map, key, 1)`;
+        auto irb = insert_before(r); // Get a handle to the LLVM IR builder
+        irb.CreateCall(DsfIncrementFunction, {DsfMapVariable, key, getConst(1)}); 
+    }
+
+    void visitCallInst (CallInst &r) {
+        auto key = createProgramLocation(); // static random value
+
+        // Insert call to `dsf_increment(dsf_map, key, 1)`;
+        auto irb = insert_before(r); // Get a handle to the LLVM IR builder
         irb.CreateCall(DsfIncrementFunction, {DsfMapVariable, key, getConst(1)}); 
     }
 };
